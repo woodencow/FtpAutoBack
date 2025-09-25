bc76988937a0ffa50413fed4981a855cd4b54f7a
BIGBIGSUI <156430535+BIGBIGSUI@users.noreply.github.com>
2025-09-25 08:58:21 +0000

    aaaaaa

---
+++ build/build/switch/CMakeFiles/ftpexe.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/24289/ftpsrv/src/log/log.c" "CMakeFiles/ftpexe.dir/src/log/log.o" "gcc" "CMakeFiles/ftpexe.dir/src/log/log.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/custom_commands.c" "CMakeFiles/ftpexe.dir/src/platform/nx/custom_commands.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/custom_commands.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/main.c" "CMakeFiles/ftpexe.dir/src/platform/nx/main.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/main.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/reboot_to_payload/ams_bpc.c" "CMakeFiles/ftpexe.dir/src/platform/nx/reboot_to_payload/ams_bpc.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/reboot_to_payload/ams_bpc.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/reboot_to_payload/reboot_to_payload.c" "CMakeFiles/ftpexe.dir/src/platform/nx/reboot_to_payload/reboot_to_payload.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/reboot_to_payload/reboot_to_payload.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/rtc/max77620-rtc.c" "CMakeFiles/ftpexe.dir/src/platform/nx/rtc/max77620-rtc.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/rtc/max77620-rtc.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/utils.c" "CMakeFiles/ftpexe.dir/src/platform/nx/utils.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/utils.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_fs.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_fs.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_fs.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_gc.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_gc.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_gc.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_hdd.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_hdd.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_hdd.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_none.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_none.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_none.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_root.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_root.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_root.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_save.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_save.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_save.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_stdio.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_stdio.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_stdio.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs/vfs_nx_storage.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_storage.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs/vfs_nx_storage.o.d"
+  "/home/24289/ftpsrv/src/platform/nx/vfs_nx.c" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs_nx.o" "gcc" "CMakeFiles/ftpexe.dir/src/platform/nx/vfs_nx.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/build/switch/CMakeFiles/ftpexe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ftpexe.
+# This may be replaced when dependencies are built.
