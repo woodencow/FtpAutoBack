8492083ef64905774fb1a1964d932ae3f8b26590
TOM (Bad EN) <1808461873@qq.com>
2025-09-26 17:26:00 +0800

    把sysftp.json改名为sysFtpAutoBack

--- build_release.sh
+++ build_release.sh
@@ -15,10 +15,10 @@ rm -rf out
 
 # --- SWITCH --- #
 echo "打包自动备份程序中..."
-mkdir -p out/sys-autoback/config/ftpsrv
-mkdir -p out/sys-autoback/atmosphere/contents/
-cp assets/config.template.ini out/sys-autoback/config/ftpsrv/
-cp -r build/switch/420000000000011B out/sys-autoback/atmosphere/contents/
+mkdir -p out/sysFtpAutoBack/config/ftpsrv
+mkdir -p out/sysFtpAutoBack/atmosphere/contents/
+cp assets/config.template.ini out/sysFtpAutoBack/config/ftpsrv/
+cp -r build/switch/420000000000011B out/sysFtpAutoBack/atmosphere/contents/
 
 echo "- 自动备份程序打包成功！"
-echo "- 输出位置: out/sys-autoback/"
+echo "- 输出位置: out/sysFtpAutoBack/"
